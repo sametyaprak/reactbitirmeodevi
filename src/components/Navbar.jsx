5d2ee9e0aa29f2cc49a0fced3b96363e8ce32130
sametyaprak <67242005+sametyaprak@users.noreply.github.com>
2021-06-17 20:43:36 +0200

    test1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,8 +10,6 @@ import {
 import logo from "../images/fb-leverage-of-coding.jpg";
 import { Link } from "react-router-dom";
 
-
-
 // !Lokal resimleri gosterilbilmek icin imporrt yontemiyle eklemek gerekiyor. Aksi taktirde resimler gozukmuyor.
 
 function MyNav() {
@@ -24,12 +22,22 @@ function MyNav() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link> <Link to="/personel1">Calisanlarimiz</Link> </Nav.Link>
-            <Nav.Link> <Link to="/personel2">Musterilerimiz</Link> </Nav.Link>
+            <Link className="nav-link" to="/personel1">
+              Calisanlarimiz
+            </Link>
+            <Link className="nav-link" to="/personel2">
+              Musterilerimiz
+            </Link>
             <NavDropdown title="Ufak Calismalar" id="basic-nav-dropdown">
-              <NavDropdown.Item href="../ufakcalismalar/sayi.html">Sayi Oyunu</NavDropdown.Item>
-              <NavDropdown.Item href="http://localhost:3000/form1">Form1</NavDropdown.Item>
-              <NavDropdown.Item href="http://localhost:3000/form2">Form2</NavDropdown.Item>
+              <NavDropdown.Item href="../ufakcalismalar/sayi.html">
+                Sayi Oyunu
+              </NavDropdown.Item>
+              <NavDropdown.Item href="http://localhost:3000/form1">
+                Form1
+              </NavDropdown.Item>
+              <NavDropdown.Item href="http://localhost:3000/form2">
+                Form2
+              </NavDropdown.Item>
             </NavDropdown>
           </Nav>
           <Form>
